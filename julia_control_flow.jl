ec136dc84a716a706e494bde140e289d2605129c
takuizum <sep10.taku.izum@gmail.com>
2019-03-27 22:12:57 +0900

    add some files
    until multi-array

---
+++ julia_control_flow.jl
@@ -0,0 +1,100 @@
+# Control Frow
+
+# compound expressions
+# use "begin" syntax or ";" symbol
+z = begin
+    x = "use begin semantics"
+    y = " and use ; symbol"
+    println(x, y)
+end
+
+# The bigin block must end 'end'
+begin x = 1; y = 2; x + y end
+(x = 1; y = 2; x + y)
+
+# conditional evaluation(関数にも入れることができる)
+x = 1
+y = 2
+if x < y
+    println("condition 1")
+elseif x > y
+    println("condition 2")
+else
+    println("condition 3")
+end
+# ただし，基本的にif blockはleaklyなので注意（Rとかと一緒）
+function test(x,y)
+           if x < y
+               relation = "less than"
+           elseif x == y
+               relation = "equal to"
+           else
+               relation = "greater than"
+           end
+           println("x is ", relation, " y.")
+       end
+test(1,2);test(2,1);test(1,1)
+# condition type
+if 1 println("true") end # Rはこれでもいけちゃうけど
+# ternary operator '?:'
+x = 2
+x == 1 ? println("true") : println("false")
+# ただしsingle expression valueのときにしか使えない。
+println(x == 1 ? "true" : "false")
+# 連結すれば，いくらでもいけちゃう
+test(x, y) = println(x < y ? "x is less than y"    :
+                     x > y ? "x is greater than y" : "x is equal to y")
+test(1,2)
+
+# short-circuit evaluation
+# && = かつ, || = または
+
+# repeated evaluation
+# while
+x = 1
+while x <= 5
+    #print(x) # print だと改行されないみたい
+    println(x)
+    global x += 1
+end
+x = 1
+while x <= 10000
+    print(x, "\r") # print だと改行されないみたい
+    global x += 1
+end
+# for
+for i = 1:100
+    print(i, "\r")
+end
+i # iはglobalには存在しない。for blockの中だけで定義されているから？
+for s ∈ ["foo","bar","baz"] # ∈の出し方はよく分からない = JUNOならば\inでいける
+           println(s)
+end
+# break と continueは一般的な使い方でいける模様
+
+# The throw function # ちょっとまだ使い方がよく分かっていない
+# See also "https://docs.julialang.org/en/v1/manual/control-flow/#Built-in-Exceptions-1"
+# 負の数が与えられた場合にエラーメッセージを吐く
+f(x) = x>=0 ? exp(-x) : throw(DomainError(x, "argument must be nonnegative"))
+f(-2)
+
+# Errors
+fussy_sqrt(x) = x >= 0 ? sqrt(x) : error("negative x not allowed")
+fussy_sqrt(-2) # "うるさい"平方根w
+
+# tyr & catch statement
+f(x) = try
+           sqrt(x)
+       catch
+           sqrt(complex(x, 0))
+end
+f(-1)
+# finally
+f(x) = try
+           sqrt(x)
+       catch
+           sqrt(complex(x, 0))
+       finally
+           println("complete!")
+end
+f(-1)
---
+++ julia_scope_and_variables.jl
@@ -0,0 +1,88 @@
+# module
+module A
+    a = 1
+end;
+
+module B
+    module C
+        c = 2
+    end
+    b = 2C.c
+    import ..A　#他のモジュールから参照する
+    d = A.a
+end;
+# "module name" . "a variable in module"
+A.a
+B.d
+C.c # これではダメ
+B.C.c
+# local and global
+for i = 1:10
+    global z # でも基本的にGlobalにおくのは避けた方がよさそう
+    z = i
+end
+z
+
+for i ∈ 1:100
+    local x # this is also default
+    x = i + 1
+end
+x
+
+# Let blocks
+# letから始まるブロックはglobal変数を受けつけるが，letの隣で新たに指定した変数をlocalに保存して参照する。
+x, y, z = -1, -1, -1;
+let x = 1, z
+    println("x: $x, y: $y") # x is local variable, y the global
+    println("z: $z") # errors as z has not been assigned yet but is local
+end
+# undef = alias for UndefInitializer(), which is undefined initializer
+Fs = Vector{Any}(undef, 3)
+i = 1 # initialize
+while i <= 3
+    Fs[i] = ()->i
+    global i += 1
+end
+Fs[1] # この指定にはλがはいってる。なぜ？
+Fs[2]()
+Fs = Vector{Any}(undef, 3)
+# ちょっとかえてみる
+i = 1 # initialize
+while i <= 3
+    Fs[i] = i
+    global i += 1
+end
+Fs[2] # ほしい変数が入ってる・
+Fs[2]() # ここには何もない
+# let blockを入れた場合
+Fs = Vector{Any}(undef, 3)
+i = 1 # initialize
+while i <= 3
+    let i = i
+        Fs[i] = ()->i
+    end
+    global i += 1
+end
+Fs[1] # この指定にはλがはいってる。なぜ？
+Fs[2]()
+# forloop comprehension
+# iはいずこに？
+for i = 1:10 end # iteration variableはいつも新しく作られるので，localやglobalには残らない
+i
+function f()
+    i = 0
+    for outer i = 1:3 # ところがouterで指定すると，localにも残る
+    end
+    return i
+end
+f()
+for outer i = 1:10 end  # これはダメ
+
+# constants
+# 基本的にC++で使ったのと一緒
+# invalid redefinition of constant variables
+x = nothing # restartingしないとだめっぽ
+const x = 1.0
+x = 1
+const y = 2.0
+t = 1.0 # もしも同じ方ならば，warningがでるが，reassignできる
