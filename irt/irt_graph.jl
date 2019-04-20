0d2a312cd8d62d1427df48722e92378fca9f2387
takuizum <sep10.taku.izum@gmail.com>
2019-04-20 12:07:26 +0900

    Update

--- irt/irt_graph.jl
+++ irt/irt_graph.jl
@@ -1,5 +1,9 @@
 using PyPlot
+using Plots
+gr()
 theta = collect(-4:0.1:4)
 icc = 1.0 ./(1.0 .+ exp.(-2.0.*(theta .- 0.0)))
 
-plot(theta, icc)
+Plots.gr()
+Plots.plot(theta, icc)
+PyPlot.plot(theta, icc)
