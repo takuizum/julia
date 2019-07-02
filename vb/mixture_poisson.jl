871cb11343d33579cd33314ea9229a3ca55ed0d4
takuizum <sep10.taku.izum@gmail.com>
2019-07-03 00:24:40 +0900

    Until just before ELBO

--- vb/mixture_poisson.jl
+++ vb/mixture_poisson.jl
@@ -1,6 +1,6 @@
 # mixture poisson
 
-using Distributions, StatsBase, StatsFuns, Plots, Random
+using Distributions, StatsBase, StatsFuns, Plots, Random, SpecialFunctions
 
 # Poisson's hyper parameter is λ
 plot(fit(Histogram, rand(Poisson(), 10000), nbins = 20))
@@ -17,19 +17,42 @@ plot!([0:0.01:20;], pdf.(Gamma(Khyper[4,1], Khyper[4,2]), [0:0.01:20;]))
 X = [ rand(Poisson(λ[Sn[i]]), 1)[1] for i in 1:length(Sn)]
 plot(fit(Histogram, X))
 
-
-function vb(X, nK)
+nK = 4
+function vb(X, nK, a, b)
     N = length(X)
+    # hyper parameters
+    a = sample([1:1:40;], nK); b = sample([1:1:40;], nK); α = sample([1:1:40;], nK)
     # initialize distribution (Expectation)
-    λ0 = fill(mean(Gamma(5, 5)), nK)
-    π0 = fill(1, nK) / nK
-    # Expectation of Sn
-    η1 = [exp(X[n]*log(λ0[k]) - λ0[k] + log(π0[k])) for n in 1:N, k in 1:nK]
-    # Expectation of λ
+    λ0 = a ./ b
+    lnλ0 = zeros(nK)
+    lnπ0 = zeros(nK)
+    for k in 1:nK
+        lnλ0[k] = digamma(a[k]) - log(b[k])
+        lnπ0[k] = digamma(α[k]) - digamma(sum(α))
+    end
+    η1 = [exp(X[n]*lnλ0[k] - λ0[k] + lnπ0[k]) for n in 1:N, k in 1:nK]
     for i in 1:N
         η1[i,:] .= η1[i,:] / sum(η1[i,:])
     end
-    a1 = zeros(1) # hyper α
-    for i in 1:N
-        a1 += mean(Categorical(η1[i,:])) * X[i]
+    # Expectation of λ and π
+
+    # a1 = zeros(nK) # hyper α
+    # sn1 = mean(η1, dims = 2)
+
+    a1 = sum(η1 .* X, dims = 1) + a'
+    b1 = sum(η1, dims = 1) + b'
+    α1 = sum(η1, dims = 1) + α'
+    λ1 = a1 ./ b1
+    lnλ1 = zeros(nK)
+    lnπ1 = zeros(nK)
+    for k in 1:nK
+        lnλ1[k] = digamma(α1[k]) - log(b1[k])
+        lnπ1[k] = digamma(α1[k]) - digamma(sum(α1))
     end
+    # Expectation of Sn
+    η1 = [exp(X[n]*lnλ1[k] - λ1[k] + lnπ1[k]) for n in 1:N, k in 1:nK]
+    # Expectation of λ and π
+    for i in 1:N
+        η1[i,:] .= η1[i,:] / sum(η1[i,:])
+    end
+    # ELBO
