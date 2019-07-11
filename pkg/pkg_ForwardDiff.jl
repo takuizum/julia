f35fec6ffaef5caae092e194198193363bdb7836
takuizum <40698749+takuizum@users.noreply.github.com>
2019-07-11 19:12:47 +0900

    add 2 packages, related AD

---
+++ pkg/pkg_ForwardDiff.jl
@@ -0,0 +1,16 @@
+# ForwardDiff
+
+using ForwardDiff
+
+function icc2pl(θ, α, β)
+    p = zeros(length(θ))
+    for i in 1:length(θ)
+        p[i] = logistic(α*(θ[i]-β))
+    end
+    p
+end
+
+θ = [-4: .1: 4;]
+
+gicc = θ -> ForwardDiff.gradient(icc2pl(θ, 1.0, 1.0))
+gicc(θ)
