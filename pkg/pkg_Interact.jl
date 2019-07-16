f4020e8763e04cbaba0783c1af2ec64cc58716af
takuizum <40698749+takuizum@users.noreply.github.com>
2019-07-16 10:59:35 +0900

    change for loop structure in @manipulate

--- pkg/pkg_Interact.jl
+++ pkg/pkg_Interact.jl
@@ -1,7 +1,7 @@
 # Interact Plots
 using Pkg; Pkg.add ORCA PlotlyJS CSSUtil Interact
 
-using Interact, Plots, StatsFuns, PlotlyJS
+using Interact, Plots, StatsFuns, PlotlyJS, ORCA, CSSUtil
 
 
 @manipulate for α in slider(0.0001:0.001:3)
@@ -13,8 +13,7 @@ end
 
 #%%
 plotlyjs()
-@manipulate for α in slider(0.0001:0.1:3)
-    @manipulate for β in slider(-4:0.1:4)
+@manipulate for α in 0.0001:0.1:3, β in -4:0.1:4
         θ = [-4:0.001:4;]
         prob = logistic.((θ.-β)*α)
         prob = logistic.((θ.-β)*α)
@@ -22,3 +21,9 @@ plotlyjs()
     end
 end
 #%%
+
+ui = @manipulate throttle = 0.1 for r = 0:.05:1, g = 0:.05:1, b = 0:.05:1
+     HTML(string("<div style='color:#", hex(RGB(r,g,b)), "'>Color me</div>"))
+ end
+ @layout! ui dom"div"(observe(_), vskip(2em), :r, :g, :b)
+ ui
