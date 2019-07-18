77e66cb879cf256c8603ef31b052906e15b90ce2
takuizum <40698749+takuizum@users.noreply.github.com>
2019-07-18 16:11:03 +0900

    set up Stan.jl

---
+++ pkg/pkg_Stan.jl
@@ -0,0 +1,36 @@
+# run below code in Shell.
+# git clone https://github.com/stan-dev/cmdstan.git --recursive
+
+using Pkg; Pkg.add("Stan") # take several minutes.
+Pkg.add("Mamba.jl") # こちらもひつよう？
+
+ENV["CMDSTAN_HOME"] = "C:\\Users\\bc0089985\\cmdstan\\"
+using Stan
+show(ENV)
+# Stan.set_cmdstan_home!("C:\\Users\\bc0089985\\cmdstan\\")
+# ENV["JULIA_CMDSTAN_HOME"] = "C:\\Users\\bc0089985\\cmdstan\\"
+
+using Mamba # This code takes several minutes too.
+
+versioninfo()
+
+# Example
+bernoullimodel = "
+data {
+  int<lower=0> N;
+  int<lower=0,upper=1> y[N];
+}
+parameters {
+  real<lower=0,upper=1> theta;
+}
+model {
+  theta ~ beta(1,1);
+  y ~ bernoulli(theta);
+}
+"
+
+stanmodel = Stanmodel(num_samples=1200, thin=2, model=bernoullimodel)
+stanmodel |> display
+dat = [Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])]
+
+rc, sim = stan(stanmodel, dat)
