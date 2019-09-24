590ed1a05cb964483a425fde9cc14567b35307a5
takuizum <40698749+takuizum@users.noreply.github.com>
2019-09-24 20:49:36 +0900

    fic function

--- mcsample.jl
+++ mcsample.jl
@@ -2,7 +2,7 @@
 
 using Plots, Distributions, Optim
 
-f(x) = (pdf(Normal(-1, 1), x) + pdf(Normal(1, 0.5), x)) / 2
+f(x) = (pdf.(Normal(-1, 1), x) + pdf.(Normal(1, 0.5), x)) / 2
 x = [-4:.1:4;]
 y = f(x)
 
@@ -10,12 +10,12 @@ plot(x, y)
 
 
 function mcsampling(n)
-    f(x) = -(pdf(Normal(-1, 1), x) + pdf(Normal(1, 0.5), x)) / 2
+    f(x) = -(pdf.(Normal(-1, 1), x) + pdf.(Normal(1, 0.5), x)) / 2
     opt = optimize(f, -4, 4)
     X = -opt.minimum*1.001
     res = zeros(n)
     t = 1 # counter
-    g(x) = (pdf(Normal(-1, 1), x) + pdf(Normal(1, 0.5), x)) / 2
+    g(x) = (pdf.(Normal(-1, 1), x) + pdf.(Normal(1, 0.5), x)) / 2
 
     xtmp = rand(Uniform(-4, 4), n) # axis x
     ytmp = rand(Uniform(0, X), n)
@@ -39,6 +39,7 @@ savefig("test.png")
 # gif animation
 
 plot(x, y, label = "")
+# Need to install `ffmpeg` before use @ gif macro.
 @gif for i in 1:10
     scatter!([smp[i,1]], [smp[i,2]], markercolor = smp[i,2] > smp[i,3] ? "red" : "blue",　label = "")
 end
