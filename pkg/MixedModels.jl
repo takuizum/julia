06c149ec96117146c8e7496e9b4cb73423697cc0
takuizum <sep10.taku.izum@gmail.com>
2019-10-15 00:29:40 +0900

    add MIxed Models

--- pkg/MixedModels.jl
+++ pkg/MixedModels.jl
@@ -1,2 +1,17 @@
 # Multilevel Models
 import Pkg; Pkg.add("MixedModels")
+using MixedModels, RDatasets, TableReader 
+
+# Convert DataFrame encoding to UTF-8 from S-JIS.
+data = readcsv("/Users/takuizum/local_Documents/julia/data/井の中の蛙.csv")
+data |> x -> first(x, 10)
+
+# Unconditional intercept model
+mod1 = LinearMixedModel(@formula(学業的自己概念~テスト得点+(1|学級)), data)
+fit(MixedModel, @formula(学業的自己概念~テスト得点+(1|学級)), data)
+
+# Unconditonal slope model
+
+
+
+
