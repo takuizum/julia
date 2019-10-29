d43756abcc7621b8e9c3ca522c03becc1379bf7c
takuizum <40698749+takuizum@users.noreply.github.com>
2019-10-29 19:52:30 +0900

    update to ignore tmp folder

--- pkg/gen_irt.jl
+++ pkg/gen_irt.jl
@@ -15,27 +15,35 @@ using Gen, Distributions, StatsFuns
     return resp
 end;
 
+# sample parameter
 a = [1.0,2.0,1.0,2.0,1.0];
 b = [-2.0:1.0:2.0;];
 θ = rand(Normal(),1_000);
+
 irt2pl_gen(θ, a, b)
 trace = Gen.simulate(irt2pl_gen, (θ, a, b,))
 Gen.get_args(trace)
 println(Gen.get_choices(trace))
-
+# generate datum
+@gen function generate_response(θ::Float64, a::Float64, b::Float64)
+    return @trace( bernoulli(StatsFuns.logistic(a*(θ-b))), (:resp, i, j) )
+end;
 # Probability model
 @gen function irt2pl(N::Int64, J::Int64)
     n = 9
-    a = @trace(uniform(0, 2), :a)
-    b = @trace(normal(0, 2), :b)
-    θ = @trace(normal(0, 1), :θ)
+    a = b = zeros(J)
+    θ = seros(N)
+    for i in 1:N
+        θ[i] = @trace(normal(0, 1), (:θ, i))
+    end
+    for j in 1:J
+        a[j] = @trace(uniform(0, 2), (:a, j))
+        b[j] = @trace(normal(0, 2), (:b, j))
+    end
+    
     for i in 1:N
         for j in 1:J
-            if @trace( bernoulli(StatsFuns.logistic(a*(θ-b))), (:resp, i, j) )
-                1
-            else
-                0
-            end
+            generate_response(θ[i], a[j], b[j])
         end
     end
     return n
@@ -66,3 +74,5 @@ data
 fit = do_inference(irt2pl, data, 1000)
 choices = Gen.get_choices(fit)
 choices[:b]
+
+
--- pkg/pkg_stan/pkg_Stan.jl
+++ pkg/pkg_stan/pkg_Stan.jl
@@ -4,15 +4,12 @@
 using Pkg; Pkg.add("Stan") # take several minutes.
 Pkg.add("Mamba") # こちらもひつよう？
 
-ENV["CMDSTAN_HOME"] = "C:\\Users\\bc0089985\\cmdstan\\"
+ENV["CMDSTAN_HOME"] = "C:\\Users\\bc0089985\\cmdstan\\cmdstan-2.21.0\\"
 ENV["CMDSTAN_HOME"] = "/Users/takuizum/cmdstan/"
-using Stan, Mamba
+using Stan, Mamba, CmdStan
 show(ENV)
 # Stan.set_cmdstan_home!("C:\\Users\\bc0089985\\cmdstan\\")
 # ENV["JULIA_CMDSTAN_HOME"] = "C:\\Users\\bc0089985\\cmdstan\\"
-
-using Mamba # This code takes several minutes too.
-
 versioninfo()
 
 # Example
@@ -30,8 +27,8 @@ model {
 }
 "
 
-stanmodel = Stanmodel(num_samples=1200, thin=2, model=bernoullimodel)
+stanmodel = CmdStan.Stanmodel(num_samples=1200, thin=2, model=bernoullimodel)
 stanmodel |> display
 dat = [Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])]
 
-rc, sim = stan(stanmodel, dat)
+rc, sim = CmdStan.stan(stanmodel, dat)
