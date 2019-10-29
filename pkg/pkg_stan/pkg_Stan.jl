993e4145d86df89bc2d190a5bc668a5f552d1e03
takuizum <40698749+takuizum@users.noreply.github.com>
2019-10-29 20:48:40 +0900

    update stan.jl The code dont' work due to g++ path

--- pkg/pkg_stan/pkg_Stan.jl
+++ pkg/pkg_stan/pkg_Stan.jl
@@ -2,12 +2,14 @@
 # git clone https://github.com/stan-dev/cmdstan.git --recursive
 
 using Pkg; Pkg.add("Stan") # take several minutes.
+using Pkg; Pkg.add("CmdStan") # take several minutes.
+
 Pkg.add("Mamba") # こちらもひつよう？
 
 ENV["CMDSTAN_HOME"] = "C:\\Users\\bc0089985\\cmdstan\\cmdstan-2.21.0\\"
 ENV["CMDSTAN_HOME"] = "/Users/takuizum/cmdstan/"
-using Mamba, Stan
-using Manba, CmdStan
+using Stan
+using CmdStan
 show(ENV)
 # Stan.set_cmdstan_home!("C:\\Users\\bc0089985\\cmdstan\\")
 # ENV["JULIA_CMDSTAN_HOME"] = "C:\\Users\\bc0089985\\cmdstan\\"
@@ -34,3 +36,8 @@ stanmodel |> display
 dat = [Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])]
 
 rc, sim = CmdStan.stan(stanmodel, dat)
+
+# Error Message is as below -------------------------------------
+# An error occurred while compiling the Stan program.
+# Please check your Stan program in variable 'bernoulli' and the contents of C:/Users/bc0089985/Documents/GitHub/julia/tmp/bernoulli.exe_build.log.
+# Note that Stan does not handle blanks in path names.
--- tmp/bernoulli.stan
+++ tmp/bernoulli.stan
@@ -1,10 +1,10 @@
-data { 
-  int<lower=1> N; 
-  int<lower=0,upper=1> y[N];
-} 
-parameters {
+data {
+  int<lower=0> N;
+    int<lower=0,upper=1> y[N];
+    }
+    parameters {
   real<lower=0,upper=1> theta;
-} 
+}
 model {
   theta ~ beta(1,1);
   y ~ bernoulli(theta);
