af5c75c3bf064b2de01eaaef752ccf8de9e0298d
takuizum <40698749+takuizum@users.noreply.github.com>
2019-11-13 13:19:38 +0900

    update plot function to show multiple trace line

--- project/irt/betaIRT.jl
+++ project/irt/betaIRT.jl
@@ -2,20 +2,26 @@
 using StatsFuns, Distributions
 
 # response function
+mutable struct brm1
+    τ::Vector{Float64}
+    β::Vector{Float64}
+end
+
 mutable struct brm2
     α::Vector{Float64}
     β::Vector{Float64}
+end
+
+mutable struct brm3
+    α::Vector{Float64}
+    β::Vector{Float64}
+    τ::Vector{Float64}
 end
 # plot output struct
 mutable struct brm_plot
     p::Vector{Float64}
     shape1::Float64
     shape2::Float64
-end
-
-mutable struct brm1
-    τ::Vector{Float64}
-    β::Vector{Float64}
 end
 
 test = brm2(rand(10), rand(11))
@@ -26,6 +32,33 @@ test.β[1] = "hoge"
 mutable struct MissingIndicator
     u::Vector{Int64}
     g::Int64
+end
+
+# BRM1
+function irf(para::brm1, θ::Float64, y::Vector{Float64}, Mis::MissingIndicator)
+    # shape1 = shape2 = zeros(eltype(y), length(y))
+    shape1 = @. exp((θ - para.β[Mis.u] + para.τ[Mis.u])/2)
+    shape2 = @. exp((para.β[Mis.u] - θ + para.τ[Mis.u])/2)
+    p = @. Distributions.pdf(Distributions.Beta(shape1, shape2), y[Mis.u])
+    return p
+end
+# BRM1(for θ base plotting)
+function irf(para::brm1, θ::Vector{Float64}, y::Float64)
+    # shape1 = shape2 = zeros(eltype(y), length(y))
+    shape1 = @. exp((θ - para.β + para.τ)/2)
+    shape2 = @. exp((para.β - θ + para.τ)/2)
+    # @show shape1 shape2
+    p = @. Distributions.pdf(Distributions.Beta(shape1, shape2), y)
+    return brm_plot(p, shape1[1], shape2[1])
+end
+# BRM1(for y base plotting)
+function irf(para::brm1, θ::Float64, y::Vector{Float64})
+    # shape1 = shape2 = zeros(eltype(y), length(y))
+    shape1 = @. exp((θ - para.β + para.τ)/2)
+    shape2 = @. exp((para.β - θ + para.τ)/2)
+    # @show shape1 shape2
+    p = @. Distributions.pdf(Distributions.Beta(shape1, shape2), y)
+    return brm_plot(p, shape1[1], shape2[1])
 end
 
 # BRM2
@@ -54,28 +87,29 @@ function irf(para::brm2, θ::Float64, y::Vector{Float64})
     p = @. Distributions.pdf(Distributions.Beta(shape1, shape2), y)
     return brm_plot(p, shape1[1], shape2[1])
 end
-# BRM1
-function irf(para::brm1, θ::Float64, y::Vector{Float64}, Mis::MissingIndicator)
+
+# BRM3
+function irf(para::brm3, θ::Float64, y::Vector{Float64}, Mis::MissingIndicator)
     # shape1 = shape2 = zeros(eltype(y), length(y))
-    shape1 = @. exp((θ - para.β[Mis.u] + para.τ[Mis.u])/2)
-    shape2 = @. exp((para.β[Mis.u] - θ + para.τ[Mis.u])/2)
+    shape1 = @. exp(para.α[Mis.u]*(θ - para.β[Mis.u] + para.τ[Mis.u])/2)
+    shape2 = @. exp(para.α[Mis.u]*(para.β[Mis.u] - θ + para.τ[Mis.u])/2)
     p = @. Distributions.pdf(Distributions.Beta(shape1, shape2), y[Mis.u])
     return p
 end
-# BRM1(for θ base plotting)
-function irf(para::brm1, θ::Vector{Float64}, y::Float64)
+# BRM3(for θ base plotting)
+function irf(para::brm3, θ::Vector{Float64}, y::Float64)
     # shape1 = shape2 = zeros(eltype(y), length(y))
-    shape1 = @. exp((θ - para.β + para.τ)/2)
-    shape2 = @. exp((para.β - θ + para.τ)/2)
+    shape1 = @. exp(para.α*(θ - para.β + para.τ)/2)
+    shape2 = @. exp(para.α*(para.β - θ + para.τ)/2)
     # @show shape1 shape2
     p = @. Distributions.pdf(Distributions.Beta(shape1, shape2), y)
     return brm_plot(p, shape1[1], shape2[1])
 end
-# BRM1(for y base plotting)
-function irf(para::brm1, θ::Float64, y::Vector{Float64})
+# BRM3(for y base plotting)
+function irf(para::brm3, θ::Float64, y::Vector{Float64})
     # shape1 = shape2 = zeros(eltype(y), length(y))
-    shape1 = @. exp((θ - para.β + para.τ)/2)
-    shape2 = @. exp((para.β - θ + para.τ)/2)
+    shape1 = @. exp(para.α*(θ - para.β + para.τ)/2)
+    shape2 = @. exp(para.α*(para.β - θ + para.τ)/2)
     # @show shape1 shape2
     p = @. Distributions.pdf(Distributions.Beta(shape1, shape2), y)
     return brm_plot(p, shape1[1], shape2[1])
@@ -88,31 +122,96 @@ using Plots, Interact, CSSUtil, ORCA, Printf
 # ICC
 function ICC_plot(mod::DataType)
     gr()
-    @manipulate for α in -2:0.001:2, β in -4:.001:4, y in 0:.001:1
-        res = irf(mod([α], [β]), [-4:.1:4;], y)
-        plot([-4:.1:4], res.p,
-             label = "ICC", xlabel = "θ", ylabel = "Probability")
-        annotate!(0, 0, "Model is $(string(mod))\nshape1 = $(@sprintf("%.3f",res.shape1)),shape2 = $(@sprintf("%.3f", res.shape2))")
+    if fieldnames(mod) == (:α, :β, :τ)
+        @manipulate for α in -0:0.001:4, β in -4:.001:4, τ in -4:.001:4, y in 0:.001:1
+            res = irf(mod([α], [β], [τ]), [-4:.1:4;], y)
+            plot([-4:.1:4], res.p,
+                 label = "ICC", xlabel = "theta", ylabel = "Probability")
+            annotate!(0, 0, "Model is $(string(mod))\nshape1 = $(@sprintf("%.3f",res.shape1)),shape2 = $(@sprintf("%.3f", res.shape2))")
+            ylims!((0,Inf))
+        end
+    elseif fieldnames(mod) == (:τ, :β)
+        @manipulate for τ in -4:0.001:4, β in -4:.001:4, y in 0:.001:1
+            res = irf(mod([τ], [β]), [-4:.1:4;], y)
+            plot([-4:.1:4], res.p,
+                 label = "ICC", xlabel = "theta", ylabel = "Probability")
+            annotate!(0, 0, "Model is $(string(mod))\nshape1 = $(@sprintf("%.3f",res.shape1)),shape2 = $(@sprintf("%.3f", res.shape2))")
+            ylims!((0,Inf))
+        end
+    elseif fieldnames(mod) == (:α, :β)
+        @manipulate for α in -0:0.001:4, β in -4:.001:4, y in 0:.001:1
+            res = irf(mod([α], [β]), [-4:.1:4;], y)
+            plot([-4:.1:4], res.p,
+                 label = "ICC", xlabel = "theta", ylabel = "Probability")
+            annotate!(0, 0, "Model is $(string(mod))\nshape1 = $(@sprintf("%.3f",res.shape1)),shape2 = $(@sprintf("%.3f", res.shape2))")
+            ylims!((0,Inf))
+        end
+    end
+end
+function ICC_plot()
+    gr()
+    @manipulate for α in -0:0.001:4, β in -4:.001:4, τ in -4:.001:4, y in 0:.001:1
+        res1 = irf(brm1([τ], [β]), [-4:.1:4;], y)
+        res2 = irf(brm2([α], [β]), [-4:.1:4;], y)
+        res3 = irf(brm3([α], [β], [τ]), [-4:.1:4;], y)
+        plot([-4:.1:4], res1.p, label = "BRM1", xlabel = "theta: latent trait", ylabel = "Probability Density")
+        plot!([-4:.1:4], res2.p, label = "BRM2")
+        plot!([-4:.1:4], res3.p, label = "BRM3")
         ylims!((0,Inf))
     end
 end
 ICC_plot(brm1)
 ICC_plot(brm2)
+ICC_plot(brm3)
+ICC_plot()
 
 # Genarative function
 plot([0:.01:1], irf(brm2(fill(1.0, 101), fill(0.0, 101)), 1.0, [0:.01:1;]))
 function Generative_plot(mod::DataType)
     gr()
-    @manipulate for α in -2:0.001:2.0, β in -4.0:.001:4.0, θ in -4.0:.001:4.0
-        res = irf(mod([α], [β]), θ, [0:.01:1;])
-        plot([0:.01:1;], res.p,
-             label = "Beta Densityy", xlabel = "θ", ylabel = "Probability")
-        annotate!(0.5, 0, "Model is $(string(mod))\nshape1 = $(@sprintf("%.3f",res.shape1)),shape2 = $(@sprintf("%.3f", res.shape2))")
+    if fieldnames(mod) == (:α, :β, :τ)
+        @manipulate for α in -0:0.001:4, β in -4:.001:4, τ in -4:.001:4, θ in -4.0:.001:4.0
+            res = irf(mod([α], [β], [τ]), θ, [0:.01:1;])
+            plot([0:.01:1;], res.p,
+                 label = "Beta Densityy", xlabel = "y: observed response", ylabel = "Probability")
+            annotate!(0.5, 0, "Model is $(string(mod))\nshape1 = $(@sprintf("%.3f",res.shape1)),shape2 = $(@sprintf("%.3f", res.shape2))")
+            ylims!((0,Inf))
+        end
+    elseif fieldnames(mod) == (:τ, :β)
+        @manipulate for τ in -4:0.001:4, β in -4:.001:4, θ in -4.0:.001:4.0
+            res = irf(mod([τ], [β]), θ, [0:.01:1;])
+            plot([0:.01:1;], res.p,
+                 label = "Beta Densityy", xlabel = "y: observed response", ylabel = "Probability")
+            annotate!(0.5, 0, "Model is $(string(mod))\nshape1 = $(@sprintf("%.3f",res.shape1)),shape2 = $(@sprintf("%.3f", res.shape2))")
+            ylims!((0,Inf))
+        end
+    elseif fieldnames(mod) == (:α, :β)
+        @manipulate for α in -2:0.001:2.0, β in -4.0:.001:4.0, θ in -4.0:.001:4.0
+            res = irf(mod([α], [β]), θ, [0:.01:1;])
+            plot([0:.01:1;], res.p,
+                 label = "Beta Densityy", xlabel = "y: observed response", ylabel = "Probability")
+            annotate!(0.5, 0, "Model is $(string(mod))\nshape1 = $(@sprintf("%.3f",res.shape1)),shape2 = $(@sprintf("%.3f", res.shape2))")
+            ylims!((0,Inf))
+        end
+    end
+end
+function Generative_plot()
+    gr()
+    @manipulate for α in -0:0.001:4, β in -4:.001:4, τ in -4:.001:4, θ in -4.0:.001:4.0
+        res1 = irf(brm1([τ], [β]), θ, [0:.01:1;])
+        res2 = irf(brm2([α], [β]), θ, [0:.01:1;])
+        res3 = irf(brm3([α], [β], [τ]), θ, [0:.01:1;])
+        plot([0:.01:1;], res1.p, label = "BRM1", xlabel = "y: observed response", ylabel = "Probability Density")
+        plot!([0:.01:1;], res2.p, label = "BRM2")
+        plot!([0:.01:1;], res3.p, label = "BRM3")
         ylims!((0,Inf))
     end
 end
+
 Generative_plot(brm1)
 Generative_plot(brm2)
+Generative_plot(brm3)
+Generative_plot()
 
 # Log pdf
 function logirf(para::brm2, θ::Float64, y::Vector{Float64}, Mis::MissingIndicator)
