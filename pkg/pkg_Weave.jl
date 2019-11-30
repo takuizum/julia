0058ff3097c370b82684997abc952ddbd8a4208e
takuizum <sep10.taku.izum@gmail.com>
2019-11-30 23:05:33 +0900

    add ipynb render code

--- pkg/pkg_Weave.jl
+++ pkg/pkg_Weave.jl
@@ -1,4 +1,6 @@
 using Weave
 
 pwd()
-weave("pkg/jmd/myfirst.jmd",doctype = "pandoc2html", out_path=:doc)
+weave("pkg/jmd/myfirst.jmd", doctype = "pandoc2html", out_path = :doc)
+# ipynb to html
+weave("jupyter/DataFrameHandling.ipynb", doctype = "pandoc2html", out_path = :doc)
