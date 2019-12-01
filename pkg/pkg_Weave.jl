18f75a57b01d8bc1a104af37ea02f818f4d56fa7
takuizum <sep10.taku.izum@gmail.com>
2019-12-01 19:39:54 +0900

    update pkg query

--- pkg/pkg_Weave.jl
+++ pkg/pkg_Weave.jl
@@ -4,3 +4,4 @@ pwd()
 weave("pkg/jmd/myfirst.jmd", doctype = "pandoc2html", out_path = :doc)
 # ipynb to html
 weave("jupyter/DataFrameHandling.ipynb", doctype = "pandoc2html", out_path = :doc)
+weave("pkg/jmd/pkg_Query.jmd", doctype = "pandoc2html", out_path = :doc)
