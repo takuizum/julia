969d7ab1d905d8ff584062c0c5ff780bf88af865
takuizum <40698749+takuizum@users.noreply.github.com>
2019-12-05 13:15:08 +0900

    apdate to kick weave

--- pkg/pkg_Weave.jl
+++ pkg/pkg_Weave.jl
@@ -5,3 +5,8 @@ weave("pkg/jmd/myfirst.jmd", doctype = "pandoc2html", out_path = :doc)
 # ipynb to html
 weave("jupyter/DataFrameHandling.ipynb", doctype = "pandoc2html", out_path = :doc)
 weave("pkg/jmd/pkg_Query.jmd", doctype = "pandoc2html", out_path = :doc)
+
+# 2019/12/05 optim study
+weave("jmd/optimStudy.jmd", doctype = "pandoc2html", out_path = :doc)
+
+weave("jmd/summaryPkg.jmd", doctype = "pandoc2html", out_path = :doc)
