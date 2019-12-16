49772604030017356af71a0e9232d8d5493c1209
takuizum <sep10.taku.izum@gmail.com>
2019-12-17 01:25:54 +0900

    update

--- man/julia_tasks.jl
+++ man/julia_tasks.jl
@@ -6,7 +6,7 @@
 
 function producer(c::Channel)
     put!(c, "start")
-    for n in 1:4
+    for n in 1:100000
         put!(c, 2n)
     end
     put!(c, "stop")
@@ -21,4 +21,26 @@ take!(chnl)
 # Channel can be used as iterable object.
 for x in Channel(producer)
     println(x)
+end
+
+# Channelsを使うと，評価を一回の`put!`実行ごとに行うことができる。
+
+# Parallel computings
+
+# Given Channels c1 and c2,
+c1 = Channel(32)
+c2 = Channel(32)
+# and a function `foo` which reads items from c1, processes the item read
+# and writes a result to c2,
+function foo()
+    while true
+        data = take!(c1)
+        [...]               # process data
+        put!(c2, result)    # write out result
+    end
+end
+
+# we can schedule `n` instances of `foo` to be active concurrently.
+for _ in 1:n
+    @async foo()
 end
