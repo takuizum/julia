13c4d9f2261d7b0f2bb5f0bd637c799b8ec6a0e2
takuizum <sep10.taku.izum@gmail.com>
2020-01-14 02:53:54 +0900

    Add differential image plot

--- Algorithm_for_Optimization/CounterPlot.jl
+++ Algorithm_for_Optimization/CounterPlot.jl
@@ -1,7 +1,7 @@
-using Plot
+using Plots
 
 f(x₁, x₂) = x₁^2 - x₂^2
 
-z = [f(x₁, x₂) for x₁ in -2:.1:2, x₂ in -2:.1:2]
-plot(-2:.1:2, -2:.1:2, z, seriestype = :heatmap, c = ColorGradient(:plasma))
-plot(-2:.1:2, -2:.1:2, z, seriestype = :wireframe)
+z = [f(x₁, x₂) for x₁ = -2:0.1:2, x₂ = -2:0.1:2]
+plot(-2:0.1:2, -2:0.1:2, z, seriestype = :heatmap, c = ColorGradient(:plasma))
+plot(-2:0.1:2, -2:0.1:2, z, seriestype = :wireframe)
