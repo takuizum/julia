6eec92b9c4fa17dfb236cbf7af47c57828867328
takuizum <40698749+takuizum@users.noreply.github.com>
2020-01-14 19:16:51 +0900

    Merger

--- Algorithm_for_Optimization/Derivatives and Gradients/CounterPlot.jl
+++ Algorithm_for_Optimization/Derivatives and Gradients/CounterPlot.jl
@@ -5,3 +5,4 @@ f(x₁, x₂) = x₁^2 - x₂^2
 z = [f(x₁, x₂) for x₁ = -2:0.1:2, x₂ = -2:0.1:2]
 plot(-2:0.1:2, -2:0.1:2, z, seriestype = :heatmap, c = ColorGradient(:plasma))
 plot(-2:0.1:2, -2:0.1:2, z, seriestype = :wireframe)
+
