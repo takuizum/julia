e64088ba79cbf714dae0f3ae508f32c10f74c500
takuizum <sep10.taku.izum@gmail.com>
2020-08-07 02:32:47 +0900

    Move out JuliaCon2020

---
+++ InstallingPackages.jl
@@ -0,0 +1,18 @@
+using Pkg
+
+pac = [
+    "Queryverse", # DataSci
+    "Optim", # Numerical Optimization
+    "Statistics", # Statistical function
+    "StatsFuns", 
+    "StatsBase", 
+    "Distributions", # Probability distribution
+    "RCall", # Calling R
+    "PyCall", 
+    "PyPlot", 
+    "Conda", 
+    "Weave", 
+    "Revise"
+]
+
+Pkg.add.(pac)
