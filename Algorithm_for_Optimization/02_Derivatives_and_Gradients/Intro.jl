dc6c9c3b414aa5870c6ea6761a451ae16f7df892
takuizum <40698749+takuizum@users.noreply.github.com>
2020-08-07 00:36:39 +0900

    Add from work env

--- Algorithm_for_Optimization/02_Derivatives_and_Gradients/Intro.jl
+++
@@ -1,2 +0,0 @@
-using Weave
-weave("Algorithm_for_Optimization/Derivatives_and_Gradients/Intro.jmd", doctype = "pandoc2html", out_path = :doc)
---
+++ Algorithm_for_Optimization/Introduction/Intro.jl
@@ -0,0 +1,2 @@
+using Weave
+weave("Intro.jmd", doctype = "pandoc2html", out_path = :doc)
